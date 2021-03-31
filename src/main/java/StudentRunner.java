1331f403ae571ccbe976cad60c929dcbcdd5a8cf
manthankhorwal <49580989+manthankhorwal@users.noreply.github.com>
2021-04-01 01:02:41 +0530

    Update StudentRunner.java

--- src/main/java/StudentRunner.java
+++ src/main/java/StudentRunner.java
@@ -1,12 +1,20 @@
 import java.io.IOException;
 import java.util.*;
-
+class example
+{
+    int print()
+    {
+       return 4; 
+    }
+}
 public class StudentRunner
 {
     public static void main(String[] args)
     {
         System.out.println("We are in jenkins");
         System.out.println("We are in jenkins");
+        example ex=new example();
+        int y=ex.print();
 
     }
   }
