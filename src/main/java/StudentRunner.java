3eebd7944a3972cd5962081f087176a71a7c3693
Manthan khorwal <manthankhorwal502@gmail.com>
2021-04-01 00:02:23 +0530

    first commit

---
+++ src/main/java/StudentRunner.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.*;
+
+public class StudentRunner
+{
+    public static void main(String[] args)
+    {
+        System.out.println("We are in jenkins");
+
+    }
+  }
